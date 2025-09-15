25a5148eda7fee5ce3761675cda8e9d1b95ca87b
Noma7code <henryah.yeb.5@gmail.com>
2025-09-15 16:07:30 +0000

    create a webserver to serve an html file using core nodejs

---
+++ webserver/index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+
+const requestHandler = (req, res) => {
+  const filepath = path.join(__dirname, "index.html");
+  if (req.url === "/index.html") {
+    const file = fs.readFile(filepath, "utf8", (err, data) => {
+      if (err) {
+        res.setHeader("content-Type", "text/plain");
+        res.statusCode = 500;
+        res.end("Server error");
+      } else {
+        res.setHeader("content-Type", "text/html");
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
+  } else {
+    res.setHeader("content-Type", "text/html");
+    res.statusCode = 404;
+    res.end("<h1>404-Page not found</h1>");
+  }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
